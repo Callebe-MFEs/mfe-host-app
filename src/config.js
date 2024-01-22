4ad0862f513a8341c3deb8d01e0b34f009dc262b
Callebe Gomes <callebe.gomes@cnhind.com>
2024-01-22 09:41:18 -0600

    simulating dynamic app configuration

--- src/config.js
+++ src/config.js
@@ -4,43 +4,7 @@
 import { importRemote } from "./import-remote";
 
 export const config = {
-  appConfig: [
-    {
-      id: 1,
-      name: "mfe-app-r",
-      url: "http://localhost:3001",
-      loadRemoteStyle: false,
-      path: importRemote(
-        "http://localhost:3001/remoteEntry.js",
-        "mfeAppR",
-        "./MFEAppR"
-      ),
-      route: "/",
-      activeWhen: (location) => location.pathname === "/",
-    },
-    {
-      id: 2,
-      name: "mfe-app-a",
-      url: "http://localhost:3002",
-      loadRemoteStyle: false,
-      path: importRemote(
-        "http://localhost:3002/remoteEntry.js",
-        "mfeAppA",
-        "./MFEAppA"
-      ),
-      route: "/mfe-app-a",
-      activeWhen: (location) => location.pathname.startsWith("/mfe-app-a"),
-    },
-    // {
-    //   id: 3,
-    //   name: "mfe-app-a",
-    //   url: "http://localhost:3003",
-    //   loadRemoteStyle: false,
-    //   path: import("mfeAppV/MFEAppV"),
-    //   route: "/mfe-app-v",
-    //   activeWhen: (location) => location.pathname.startsWith("/mfe-app-v"),
-    // },
-  ],
+  appConfig: window.MFEs,
   apiEndpoint: "/",
   layoutConfig: {
     layout: "cnh-root-layout-default-vertical",
--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
 import "./index.css";
 
 // import("./single-spa-only");
-import { registerAndStartApplications } from "@accelerators/psca-mfe-sdk-core";
+import { importRemote } from "./import-remote";
 
-registerAndStartApplications();
+window.MFEs = [
+  {
+    id: 1,
+    name: "mfe-app-r",
+    url: "http://localhost:3001",
+    loadRemoteStyle: false,
+    path: importRemote(
+      "http://localhost:3001/remoteEntry.js",
+      "mfeAppR",
+      "./MFEAppR"
+    ),
+    route: "/",
+    activeWhen: (location) => location.pathname === "/",
+  },
+  {
+    id: 2,
+    name: "mfe-app-a",
+    url: "http://localhost:3002",
+    loadRemoteStyle: false,
+    path: importRemote(
+      "http://localhost:3002/remoteEntry.js",
+      "mfeAppA",
+      "./MFEAppA"
+    ),
+    route: "/mfe-app-a",
+    activeWhen: (location) => location.pathname.startsWith("/mfe-app-a"),
+  },
+  // {
+  //   id: 3,
+  //   name: "mfe-app-a",
+  //   url: "http://localhost:3003",
+  //   loadRemoteStyle: false,
+  //   path: import("mfeAppV/MFEAppV"),
+  //   route: "/mfe-app-v",
+  //   activeWhen: (location) => location.pathname.startsWith("/mfe-app-v"),
+  // },
+];
+
+window.setTimeout(async () => {
+  // lazy loading the SDK causes the config.js file to have access to the window.MFEs variable;
+  const { registerAndStartApplications } = await import(
+    "@accelerators/psca-mfe-sdk-core"
+  );
+  registerAndStartApplications();
+}, 1000);
